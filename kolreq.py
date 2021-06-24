962bfc9a0bce01509ac7142ab8d00f8af9bac067
KoleckOLP-work <o.kurz@atlas.cz>
2021-06-24 12:13:24 +0200

    Fix Style and typos

--- kolreq.py
+++ kolreq.py
@@ -1,32 +1,35 @@
-from getch import getch #py-getch
 import os
+from getch import getch  # py-getch
 
-#==========MUSTYPLAT CLEAR==========#
+
+# ==========Multiplatform Clear========== #
 def clear():
-    if(os.name == 'nt'): #Windows
+    if(os.name == 'nt'):  # Windows
         os.system('cls')
-    elif(os.name == 'posix'): #Linux,Mac,BSD,haiku,android
+    elif(os.name == 'posix'):  # Linux,Mac,BSD,haiku,android
         os.system('clear')
 
-#==========MULTYPLAT READKEY==========#
-def readchar(o=""): #multiplatform readchar
-    print(o, end="", flush=True) #writes text before the getch, no new line, flush output
+
+# ==========Multiplatform Readkey========== #
+def readchar(o=""):  # multiplatform readchar
+    print(o, end="", flush=True)  # writes text before the getch, no new line, flush output
     x = getch()
-    if isinstance(x, bytes): #fix if returned in bytes, need to fix when input is arrowkeys
-        try: #fixes bug where one key sending multiple characters caused decode error
+    if isinstance(x, bytes):  # fix if returned in bytes, need to fix when input is arrow keys
+        try:  # fixes bug where one key sending multiple characters caused decode error
             x = x.decode("UTF-8")
         except UnicodeDecodeError:
             x = ' '
     x = x.lower()
     return x  
 
-#==========Romoving .0 from floats==========#	
+
+# ==========Removing .0 from floats========== #
 def n(a=""):
     if(isinstance(a, float)):
         a = str(a)
-        if(a[-2:] == ".0"): #if last 2 characters are .0 remove then
+        if(a[-2:] == ".0"):  # if last 2 characters are .0 remove then
             return(a[:-2])
         else:
-            return(a) #if not return the number in original form
+            return(a)  # if not return the number in original form
     else:
         return(a)
