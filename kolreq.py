caba29d43da4bd2c9616901eed32ae158967a097
KoleckOLP-work <o.kurz@atlas.cz>
2021-10-15 12:32:08 +0200

    Add arrowkeys support to readchar

--- kolreq.py
+++ kolreq.py
@@ -11,16 +11,33 @@ def clear():
 
 
 # ==========Multiplatform Readkey========== #
-def readchar(o=""):  # multiplatform readchar
-    print(o, end="", flush=True)  # writes text before the getch, no new line, flush output
-    x = getch()
-    if isinstance(x, bytes):  # fix if returned in bytes, need to fix when input is arrow keys
-        try:  # fixes bug where one key sending multiple characters caused decode error
-            x = x.decode("UTF-8")
-        except UnicodeDecodeError:
-            x = ' '
-    x = x.lower()
-    return x  
+def readchar(shell="", enter=False):  # multiplatform readchar
+    print(shell, end="", flush=True)  # writes text before the getch, no new line, flush output
+    x = []
+    while(True):
+        x.append(getch())
+        if x[0] != b'\xe0':  # checks if first byte is the not arrow keys
+            x = x[0]
+            if enter:
+                if x == b'\r':
+                    return "enter"
+            try:
+                x = x.decode("UTF-8")  # does fail at "ěščřžýáíé"
+            except UnicodeDecodeError:
+                x = "error"
+            return x
+
+        if len(x) == 2:  # this implementation is probably not multiplatform
+            x = x[0]+x[1]
+            if x == b'\xe0H':
+                x = "up_arrow"
+            elif x == b'\xe0P':
+                x = "down_arrow"
+            elif x == b'\xe0K':
+                x = "left_arrow"
+            elif x == b'\xe0M':
+                x = "right_arrow"
+            return x
 
 
 # ==========Removing .0 from floats========== #
